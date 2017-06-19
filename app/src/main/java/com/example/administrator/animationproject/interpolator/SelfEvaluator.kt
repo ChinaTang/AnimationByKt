9b33f81793adbeaf7ead12e377ad52b334178f3e
ChinaTang <tangdi0802@foxmail.com>
2017-06-19 21:22:01 +0800

    注释

--- app/src/main/java/com/example/administrator/animationproject/interpolator/SelfEvaluator.kt
+++ app/src/main/java/com/example/administrator/animationproject/interpolator/SelfEvaluator.kt
@@ -4,8 +4,10 @@ import android.animation.TypeEvaluator
 
 /**
  * Created by Administrator on 2017/6/19.
+ * 返回的是动画实际偏移量
  */
 class SelfEvaluator: TypeEvaluator<Int>{
-    override fun evaluate(fraction: Float, startValue: Int?, endValue: Int?): Int = (startValue!! + fraction * (endValue!! - startValue!!)) as Int
+    override fun evaluate(fraction: Float, startValue: Int?, endValue: Int?): Int
+            = (startValue!! + fraction * (endValue!! - startValue!!)) as Int
 
 }
--- app/src/main/java/com/example/administrator/animationproject/interpolator/SelfInterpolator.kt
+++ app/src/main/java/com/example/administrator/animationproject/interpolator/SelfInterpolator.kt
@@ -4,6 +4,7 @@ import android.animation.TimeInterpolator
 
 /**
  * Created by Administrator on 2017/6/19.
+ * 插值器返回的是动画的时间线百分比
  */
 class SelfInterpolator: TimeInterpolator{
     override fun getInterpolation(input: Float): Float {
